ed4a0a2c23c08e8bdf7ab8de7f6b14be5a95fafa
OLEG <olegemail@mail.ru>
2024-05-20 12:44:10 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace PSA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.менюToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.справкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.downloadData = new System.Windows.Forms.Button();
+            this.pageSetupDialog1 = new System.Windows.Forms.PageSetupDialog();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.менюToolStripMenuItem,
+            this.справкаToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(998, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // менюToolStripMenuItem
+            // 
+            this.менюToolStripMenuItem.Name = "менюToolStripMenuItem";
+            this.менюToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.менюToolStripMenuItem.Text = "Меню";
+            // 
+            // справкаToolStripMenuItem
+            // 
+            this.справкаToolStripMenuItem.Name = "справкаToolStripMenuItem";
+            this.справкаToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.справкаToolStripMenuItem.Text = "Справка";
+            // 
+            // downloadData
+            // 
+            this.downloadData.Location = new System.Drawing.Point(12, 27);
+            this.downloadData.Name = "downloadData";
+            this.downloadData.Size = new System.Drawing.Size(156, 40);
+            this.downloadData.TabIndex = 1;
+            this.downloadData.Text = "Загрузить данные";
+            this.downloadData.UseVisualStyleBackColor = true;
+            this.downloadData.Click += new System.EventHandler(this.downloadData_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.dataGridView2);
+            this.panel1.Controls.Add(this.chart1);
+            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Location = new System.Drawing.Point(12, 73);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(959, 491);
+            this.panel1.TabIndex = 2;
+            // 
+            // chart1
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart1.Legends.Add(legend3);
+            this.chart1.Location = new System.Drawing.Point(452, 21);
+            this.chart1.Name = "chart1";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(491, 293);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 21);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(433, 245);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(13, 320);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(902, 150);
+            this.dataGridView2.TabIndex = 2;
+            this.dataGridView2.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(998, 592);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.downloadData);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem менюToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem справкаToolStripMenuItem;
+        private System.Windows.Forms.Button downloadData;
+        private System.Windows.Forms.PageSetupDialog pageSetupDialog1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//using iTextSharp.text;
+//using iTextSharp.text.pdf;
+using System.IO;
+//using Microsoft.Office.Interop;
+//using Excel = Microsoft.Office.Interop.Excel;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Diagnostics;
+
+namespace PSA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<Data> Data = new List<Data>();
+
+        private void OpenFile()
+        {
+            dataGridView2.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView2.RowCount = 12;
+            dataGridView1.Rows.Clear();
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "CSV файл (*.csv)|*.csv";
+            ofd.FileName = "";
+            ofd.Title = "Открыть";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    
+                    using (var reader = new StreamReader(ofd.FileName))
+                    {
+                        string line;
+                        while ((line = reader.ReadLine()) != null)
+                        {
+                            string[] parts = line.Split(';'); // Разделить строку по запятой
+
+                            if (parts.Length >= 2)
+                            {
+                                if (double.TryParse(parts[0], NumberStyles.Any, CultureInfo.InvariantCulture, out double value)
+                                    && DateTime.TryParseExact(parts[1].Trim(), "yyMMdd", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime date))
+                                {
+                                    Data.Add(new Data { Value = value, Date = date });
+                                }
+                                else
+                                {
+                                    Console.WriteLine($"Ошибка при разборе строки: {line}");
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine($"Некорректный формат строки: {line}");
+                            }
+                        }
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            foreach(var i in Data)
+            {
+                Console.WriteLine(i.Value + " " + i.Date);
+            }
+            DrawChart(Data);
+            Razchet();
+           
+        }
+
+        private void Razchet()
+        {
+            //среднее значение
+            double CalculateMean(List<Data> dataList)
+            {
+                if (dataList.Count == 0)
+                    return 0;
+
+                double sum = 0;
+                foreach (var data in dataList)
+                {
+                    sum += data.Value;
+                }
+
+                return sum / dataList.Count;
+            }
+
+
+            //медиана
+            double CalculateMedian(List<Data> dataList)
+            {
+                if (dataList.Count == 0)
+                    return 0;
+
+                List<double> sortedValues = dataList.Select(data => data.Value).OrderBy(value => value).ToList();
+
+                int n = sortedValues.Count;
+                if (n % 2 == 1)
+                {
+                    return sortedValues[n / 2];
+                }
+                else
+                {
+                    return (sortedValues[n / 2 - 1] + sortedValues[n / 2]) / 2.0;
+                }
+            }
+
+
+            //дисперсия
+            double CalculateVariance(List<Data> dataList)
+            {
+                if (dataList.Count == 0)
+                    return 0;
+
+                double mean = CalculateMean(dataList);
+                double sumOfSquares = 0;
+
+                foreach (var data in dataList)
+                {
+                    sumOfSquares += Math.Pow(data.Value - mean, 2);
+                }
+
+                return sumOfSquares / dataList.Count;
+            }
+
+            //левый предел
+            double CalculateLeftBound(List<Data> dataList)
+            {
+                if (dataList.Count == 0)
+                    return 0;
+
+                return dataList.Min(data => data.Value);
+            }
+            //правый предел
+            double CalculateRightBound(List<Data> dataList)
+            {
+                if (dataList.Count == 0)
+                    return 0;
+
+                return dataList.Max(data => data.Value);
+            }
+
+
+            //относительная частота
+            Dictionary<double, double> CalculateRelativeFrequency(List<Data> dataList)
+            {
+                Dictionary<double, double> frequencyMap = new Dictionary<double, double>();
+                int totalCount = dataList.Count;
+
+                foreach (var data in dataList)
+                {
+                    if (frequencyMap.ContainsKey(data.Value))
+                    {
+                        frequencyMap[data.Value] += 1.0 / totalCount;
+                    }
+                    else
+                    {
+                        frequencyMap[data.Value] = 1.0 / totalCount;
+                    }
+                }
+
+                return frequencyMap;
+            }
+
+            dataGridView2.Rows[0].Cells[0].Value = "Среднее значение: " + Math.Round(CalculateMean(Data), 2);
+            dataGridView2.Rows[1].Cells[0].Value = "Медиана: " + Math.Round(CalculateMedian(Data), 2);
+            dataGridView2.Rows[2].Cells[0].Value = "Дисперсия: " + Math.Round(CalculateVariance(Data), 2);
+            dataGridView2.Rows[3].Cells[0].Value = "Левый предел: " + Math.Round(CalculateLeftBound(Data), 2);
+            dataGridView2.Rows[4].Cells[0].Value = "Правый предел: " + Math.Round(CalculateRightBound(Data), 2);
+            dataGridView2.Rows[5].Cells[0].Value = "Относит. частота: ";
+
+            foreach(var i in CalculateRelativeFrequency(Data))
+            {
+                dataGridView2.Rows[6].Cells[0].Value = dataGridView2.Rows[6].Cells[0].Value + i.Value.ToString() + "; ";
+            }
+
+
+            Console.WriteLine(CalculateMean(Data) + "        " + CalculateMedian(Data)
+                + "           " + CalculateVariance(Data) + "          " + CalculateLeftBound(Data) + "        " + CalculateRightBound(Data)
+                + "           " + CalculateRelativeFrequency(Data));
+
+            //dataGridView2.Rows[0].Cells[0].Value = Convert.ToString("Средняя арифметическая взвешенная: ") + Math.Round(Convert.ToDouble(sredVelich), 2);
+
+        }
+
+
+        private void DrawChart(List<Data> DataList)
+        {
+            // Очистить существующие серии данных на графике
+            chart1.Series.Clear();
+
+            // Создать новую серию данных для графика
+            Series series = new Series("Data Series");
+            series.ChartType = SeriesChartType.Line; // Выбрать тип графика (линейный)
+
+            // Добавить данные в серию
+            foreach (var data in DataList)
+            {
+                series.Points.AddXY(data.Date, data.Value);
+            }
+
+            // Добавить серию данных на график
+            chart1.Series.Add(series);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void downloadData_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.RowCount > 0)
+            {
+                DialogResult result = MessageBox.Show("Открыть новый файл?\nВсе подсчеты будут потеряны!.", "Сообщение",
+                 MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (result == DialogResult.Yes)
+                {
+                    OpenFile();
+
+                }
+
+            }
+            else
+            {
+                OpenFile();
+               
+            }
+            
+
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
